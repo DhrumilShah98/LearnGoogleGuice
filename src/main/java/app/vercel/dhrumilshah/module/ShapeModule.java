f16dd8beb21531307f9222fc630aa50ce99ca9d1
Dhrumil Shah <shah.dhrumil1998@gmail.com>
2025-11-18 18:52:41 -0500

    Use @Provides with @Singleton to create multiple shapes

--- src/main/java/app/vercel/dhrumilshah/module/ShapeModule.java
+++ src/main/java/app/vercel/dhrumilshah/module/ShapeModule.java
@@ -6,6 +6,7 @@ import app.vercel.dhrumilshah.shape.DrawShape;
 import app.vercel.dhrumilshah.shape.DrawSquare;
 import com.google.inject.AbstractModule;
 import com.google.inject.Provides;
+import com.google.inject.Singleton;
 
 public class ShapeModule extends AbstractModule {
 
@@ -21,6 +22,7 @@ public class ShapeModule extends AbstractModule {
     }
 
     @Provides
+    @Singleton
     @Square
     private DrawShape provideDefaultDrawSquare(@DefaultShapeColor String color,
                                                @DefaultEdge Integer edge) {
@@ -29,6 +31,7 @@ public class ShapeModule extends AbstractModule {
     }
 
     @Provides
+    @Singleton
     @Circle
     private DrawShape provideDefaultDrawCircle(@DefaultShapeColor String color,
                                                @DefaultRadius Integer radius) {
