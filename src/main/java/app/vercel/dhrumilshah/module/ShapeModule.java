2a724b33246d094e25a050eed0c5474f3cede5dd
Dhrumil Shah <shah.dhrumil1998@gmail.com>
2025-11-18 18:18:47 -0500

    Use @Provides with constants to create a default square

--- src/main/java/app/vercel/dhrumilshah/module/ShapeModule.java
+++ src/main/java/app/vercel/dhrumilshah/module/ShapeModule.java
@@ -5,6 +5,8 @@ import app.vercel.dhrumilshah.shape.DrawSquare;
 import com.google.inject.AbstractModule;
 import com.google.inject.Provides;
 import com.google.inject.Singleton;
+import com.google.inject.name.Named;
+import com.google.inject.name.Names;
 
 public class ShapeModule extends AbstractModule {
 
@@ -12,10 +14,17 @@ public class ShapeModule extends AbstractModule {
         System.out.println("Instantiating ShapeModule object.");
     }
 
+    @Override
+    protected void configure() {
+        bind(String.class).annotatedWith(Names.named("DefaultShapeColor")).toInstance("BLACK");
+        bind(Integer.class).annotatedWith(Names.named("DefaultSquareEdge")).toInstance(40);
+    }
+
     @Provides
     @Singleton
-    private DrawShape provideDefaultDrawSquare() {
-        System.out.println("Providing a default DrawSquare object.");
-        return new DrawSquare("BLACK", 40);
+    private DrawShape provideDefaultDrawSquare(@Named("DefaultShapeColor") String color,
+                                               @Named("DefaultSquareEdge") Integer edge) {
+        System.out.println("Providing a default DrawSquare object of color " + color + " and edge " + edge + ".");
+        return new DrawSquare(color, edge);
     }
 }
