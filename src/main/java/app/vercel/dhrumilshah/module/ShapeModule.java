d4f00adf7bc3640af17cbfd64ab1dc10afa16789
Dhrumil Shah <shah.dhrumil1998@gmail.com>
2025-11-18 18:11:59 -0500

    Use @Provides with @Singleton to create a default square

--- src/main/java/app/vercel/dhrumilshah/module/ShapeModule.java
+++ src/main/java/app/vercel/dhrumilshah/module/ShapeModule.java
@@ -4,6 +4,7 @@ import app.vercel.dhrumilshah.shape.DrawShape;
 import app.vercel.dhrumilshah.shape.DrawSquare;
 import com.google.inject.AbstractModule;
 import com.google.inject.Provides;
+import com.google.inject.Singleton;
 
 public class ShapeModule extends AbstractModule {
 
@@ -12,6 +13,7 @@ public class ShapeModule extends AbstractModule {
     }
 
     @Provides
+    @Singleton
     private DrawShape provideDefaultDrawSquare() {
         System.out.println("Providing a default DrawSquare object.");
         return new DrawSquare("BLACK", 40);
